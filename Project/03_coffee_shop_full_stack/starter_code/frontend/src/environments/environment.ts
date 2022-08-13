3b0a77244fe3a6e002abc10a1ff464c8f7cb1f4c
kazeem adewole <kazeemadewole@kazeems-MacBook-Pro.local>
2022-08-13 14:57:22 +0100

    added frontend environmental variables

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-ulzpoitd.us.auth0.com', // the auth0 domain prefix
+    audience: 'https://127.0.0.1:8080/api', // the audience set for the auth0 app
+    clientId: 'Hp4QhiYLkEsnVSFJyM1wAKSv103ncTVZ', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
